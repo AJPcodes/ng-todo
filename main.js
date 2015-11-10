0b253e3b9748f546ca0c9c865635dc46cc045490
Allen Phelps <allenatmc@gmail.com>
2015-11-09 19:38:46 -0600

    add two-way data binding example

--- main.js
+++ main.js
@@ -4,5 +4,10 @@ var app = angular.module('TodoApp',[])
 //make a controller
 //$scope adds variables to the DOM
 app.controller('TodoCtrl', function($scope){
-	$scope.title = "Welcome to your Angular Todo App"
+	$scope.title = "Welcome to your Angular Todo App";
+})
+
+app.controller("PastaCtrl", function($scope){
+	$scope.pasta = "Linguini";
+	$scope.foods="";
 })
